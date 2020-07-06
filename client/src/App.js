645e0b641693f3358b02858f973133ac86dd421b
Maria <mariagildacasta@gmail.com>
2020-07-06 17:56:25 +0200

    changes to front end home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./home.jsx";
+import Restaurant from "./restaurant.jsx";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Home></Home>
+          </Route>
+          <Route path="/restaurant" exact>
+            <Restaurant></Restaurant>
           </Route>
         </Switch>
       </Router>
