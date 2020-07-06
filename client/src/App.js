2db576db8641a3dbe812c6d6410f35c21cefab2b
Marta Giménez Galacho <Marta@Andrews-MacBook-Air.local>
2020-07-06 18:33:55 +0200

    auth try 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import "./App.css";
-import Home from "./home.jsx";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import Home from './home.jsx';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Home></Home>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Switch>
+					<Route path='/' exact>
+						<Home></Home>
+					</Route>
+					{/* ADD LOGIN COMPONENT */}
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
